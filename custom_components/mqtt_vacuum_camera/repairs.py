844d3df4d6a1199a9e4ac1f361bfc109f90c772d
SCA075 <82227818+sca075@users.noreply.github.com>
2026-04-25 15:43:23 +0200

    simplify repairs.py
    Signed-off-by: SCA075 <82227818+sca075@users.noreply.github.com>

--- custom_components/mqtt_vacuum_camera/repairs.py
+++ custom_components/mqtt_vacuum_camera/repairs.py
@@ -2,30 +2,8 @@
 
 from __future__ import annotations
 
-from homeassistant import data_entry_flow
 from homeassistant.components.repairs import ConfirmRepairFlow, RepairsFlow
 from homeassistant.core import HomeAssistant
-import voluptuous as vol
-
-
-class Issue1RepairFlow(RepairsFlow):
-    """Handler for an issue fixing flow."""
-
-    async def async_step_init(
-        self, _user_input: dict[str, str] | None = None
-    ) -> data_entry_flow.FlowResult:
-        """Handle the first step of a fix flow."""
-
-        return await self.async_step_confirm()
-
-    async def async_step_confirm(
-        self, user_input: dict[str, str] | None = None
-    ) -> data_entry_flow.FlowResult:
-        """Handle the confirm step of a fix flow."""
-        if user_input is not None:
-            return self.async_create_entry(title="", data={})
-
-        return self.async_show_form(step_id="confirm", data_schema=vol.Schema({}))
 
 
 async def async_create_fix_flow(
@@ -36,5 +14,3 @@ async def async_create_fix_flow(
     """Create flow."""
     if issue_id == "reset_trims_deprecated":
         return ConfirmRepairFlow()
-    if issue_id == "issue_1":
-        return Issue1RepairFlow()
